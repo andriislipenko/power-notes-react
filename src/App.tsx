59d9785e5e1110943a1890ebec70a24805e6051a
andriislipenko <andrii.slipenko@gmail.com>
2020-08-15 19:44:58 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
 import './App.scss';
+import React from 'react';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import Notes from './notes/notes';
 
-function App() {
-  return (<h1>Hello</h1>);
+function App(): JSX.Element {
+    return (
+        <BrowserRouter>
+            <Redirect from="/" to="/notes"></Redirect>
+            <Switch>
+                <Route path="/notes" component={Notes}/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/notes/notes.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Notes(): JSX.Element {
+    return (<h1>notes</h1>);
+}
+
+export default Notes;
